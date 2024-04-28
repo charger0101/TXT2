b77f3959e20c57441a90e5840fe071d43c7938f7
charger0101 <161550181+charger0101@users.noreply.github.com>
2024-04-28 11:41:37 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "16494736"
 api_hash = "2cb7fa5859e2de684e3e10d9c049621a"
-bot_token = "7062808561:AAH4aZ8UcQiWX7XBlzU8g-TaFbRN3Owagt0"
+bot_token = "6752115248:AAFpBSALor8q3QDfroMLwhSMhXEDVqxDMC4"
